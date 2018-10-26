9e47417285020e05e3da070efa47c22ca419fecd
Aswin Bhim Nath <aswismail@gmail.com>
2018-10-26 09:32:05 +0530

    Homepage Class

---
+++ lib/home_page.dart
@@ -0,0 +1,8 @@
+library crazy_calc.home_page;
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new HomePageState();
+}
